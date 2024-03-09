77d76be40a0384b94995f5ac0a287b637615d835
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-03-09 14:55:58 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,15 +1,13 @@
-# without replace
-
 string = input()
 N = len(string)
 
 ans = N
-for i in range(N):
-    if i==0: continue
-    if string[i-1:i+1] in ['c=','c-','d-','lj','nj','s=','z=']:
-        ans -= 1
 
-    if i==1: continue
+
+
+for i in range(N):
+    if i>=1 and string[i-1:i+1] in ['c=','c-','d-','lj','nj','s=','z=']:   
+        ans -= 1
     if i>=2 and string[i-2:i+1] == 'dz=':
         ans -= 1
 
