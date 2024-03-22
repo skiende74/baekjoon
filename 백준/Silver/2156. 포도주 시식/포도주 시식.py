f7c765d4c2d17e6fe50f2417c44ec2fcf75c03c4
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-03-23 00:52:42 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 seq = [int(input()) for _ in range(N)]
-dp = [[0,0,0] for _ in range(N)]
-dp[0] = [0, seq[0],seq[0]]
+dp = [0 for _ in range(N+3)]
+dp[0] = seq[0]
 for i in range(1,N):
-    dp[i][2] = dp[i-1][1] + seq[i]
-    dp[i][1] = dp[i-1][0] + seq[i]
-    dp[i][0] = max(dp[i-1])
-
-print(max(dp[-1]))
+    dp[i] = max(dp[i-3]+seq[i-1]+seq[i], dp[i-2]+seq[i])
+    dp[i] = max(dp[i], dp[i-1])
+print(dp[N-1])
