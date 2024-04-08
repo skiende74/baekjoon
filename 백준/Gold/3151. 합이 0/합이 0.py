d9f6b71f3a5113209bf47e488144f9999fd012ad
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-08 10:46:48 +0900

    [Gold IV] Title: 합이 0, Time: 7152 ms, Memory: 113380 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -2,19 +2,7 @@ from bisect import bisect_left, bisect_right
 N = int(input())
 seq = list(map(int, input().split()))
 seq.sort()
-seq_inv = seq.copy()[::-1]
 
-def custom(seq, goal):
-    left, right = 0, len(seq)-1
-    max_idx = -1
-    while left <= right:
-        mid = (left + right) //2
-        if seq[mid] <= goal: 
-            max_idx = max(max_idx, mid)
-            left = mid + 1
-        else: right = mid - 1
-    return max_idx
-    
 answer = 0
 for i in range(N-1):
     for j in range(i+1, N-1):
@@ -23,5 +11,4 @@ for i in range(N-1):
         u = bisect_right(seq, -u1-u2)
         l = max(j+1, bisect_left(seq,-u1-u2))
         answer += u-l
-        # print(i,j,j+1,l,u)
 print(answer)
