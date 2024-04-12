7b43695ebf766c563401c06ce7e742430641d25d
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-12 22:55:20 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 68552 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,30 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+readlist = lambda:list(map(int,input().split()))
+N, M = readlist()
+k = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    i,j,d = readlist()
+    graph[i].append((j, d))
+MAX=sys.maxsize
+dist = [MAX]*(N+1)
+def dijkstra(start):
+    pq = [(0,start)]
+    dist[start] = 0
+
+    while pq:
+        d, i = heappop(pq)
+        if d != dist[i]:
+            continue
+
+        for i2, d2 in graph[i]:
+            if d + d2 < dist[i2]:
+                dist[i2] = d+d2
+                heappush(pq, (dist[i2], i2))
+
+dijkstra(k)
+dist = list(map(lambda x: str(x) if x<MAX else 'INF', dist))
+print('\n'.join(dist[1:]))
