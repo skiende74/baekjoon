32a565139d2ff027609359bd01d939501ed595d5
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-20 00:30:00 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,8 +1,3 @@
 function solution(array, n) {
-    let count=0;
-    for (let a of array){
-        if(a === n)
-            count++;
-    }
-    return count;
+    return array.filter(el=>el===n).length;
 }
