660daa33097b2ca2ed9c2df897d44ec80eb8e41e
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-05-12 09:16:07 +0900

    [Gold IV] Title: RGB거리 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -4,22 +4,17 @@ input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 grid = [list(map(int,input().split())) for _ in range(N)]
 
-dp = [[[10**9]*3 for _ in range(N)] for _ in range(3)]
-dp[0][0][0] = grid[0][0]
-dp[1][0][1] = grid[0][1]
-dp[2][0][2] = grid[0][2]
-
-for k in range(3):
-    for i in range(1, N-1):
-        for j in range(3):
-            for j0 in range(3):
-                if j==j0: continue
-                dp[k][i][j] = min(dp[k][i][j], dp[k][i-1][j0] + grid[i][j])
-
 ans = 10**9
-for k in range(3):
-    for i in range(3):
-        for j in range(3):
-            if k == i or k==j: continue
-            ans = min(ans, dp[i][N-2][j] + grid[N-1][k])
+
+for l in range(3):
+    dp = [[10**9]*3 for _ in range(N)]
+    dp[0][l] = grid[0][l]    
+    for i in range(1, N):
+        dp[i][0] = grid[i][0] + min(dp[i-1][2], dp[i-1][1]) 
+        dp[i][1] = grid[i][1] + min(dp[i-1][0], dp[i-1][2]) 
+        dp[i][2] = grid[i][2] + min(dp[i-1][1], dp[i-1][0]) 
+    
+    for j in range(3):
+        if l == j: continue
+        ans = min(ans, dp[-1][j])
 print(ans)
