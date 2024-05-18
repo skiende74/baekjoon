7359cfbc3aedcb5a3e491e945a4d98cd2aec7480
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-05-18 15:32:17 +0900

    [Silver II] Title: 나무 자르기, Time: 608 ms, Memory: 264200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,18 +1,17 @@
-N, M = map(int,input().split())
+
+N, goal = map(int,input().split())
 seq = list(map(int,input().split()))
 
-left, right = 0, max(seq)
-while left <= right:
-    mid = (left + right)//2
+def possible(cut):
+    return sum([ max(0, s-cut) for s in seq]) >= goal
 
-    res = 0
-    res = sum(map(lambda x: x-mid, filter(lambda x: x>mid, seq)))
-    #for s in seq:
-    #    res += max(0, s-mid)
-    if res < M:
-        right = mid-1
-    else:
-        left = mid+1
-print(right)
+l, r = 0, 10**9
+while l < r:
+    mid = (l + r) // 2
 
+    if possible(mid):
+        l = mid+1
+    else: 
+        r = mid
 
+print(l-1)
