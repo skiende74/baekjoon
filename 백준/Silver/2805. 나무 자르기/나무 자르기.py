e66bcfcdbb5ea561c6510b4c896079c01a0d2a52
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-05-18 15:58:58 +0900

    [Silver II] Title: 나무 자르기, Time: 600 ms, Memory: 264200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,17 +1,13 @@
-
 N, goal = map(int,input().split())
-seq = list(map(int,input().split()))
+seq = list(map(int, input().split()))
 
 def possible(cut):
-    return sum([ max(0, s-cut) for s in seq]) >= goal
-
+    return sum([ max(s-cut, 0) for s in seq]) >= goal
 l, r = 0, 10**9
 while l < r:
-    mid = (l + r) // 2
-
+    mid = (l + r +1) // 2
     if possible(mid):
-        l = mid+1
-    else: 
-        r = mid
-
-print(l-1)
+        l = mid
+    else:
+        r = mid-1
+print(l)
