478c9bd69d3115b8f5285973c475c84dac31c4e8
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-05 00:28:30 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+N = int(input())
+A=list(map(int, input().split()))
+B=list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+total = 0
+for a,b in zip(A,B):
+    total+=a*b
+print(total)
