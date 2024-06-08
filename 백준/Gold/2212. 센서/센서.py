f6eb357e974a94c5af0d6a4787144bd54ce568f7
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-09 06:23:05 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32656 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -9,12 +9,4 @@ for s in seq[1:]:
     seq2.append(s - prev)
     prev = s
 seq2.sort()
-# 9간격 -> 5개선택 = 4개지우기
-# N-1개에서. M-1개를 지울수있음 -> N-M 개를 고르는 것
- 
-#  2 3 1 2
-# 1 3 6 7 9
-
-
-# print(seq2)
 print(sum(seq2[:N-M]))
