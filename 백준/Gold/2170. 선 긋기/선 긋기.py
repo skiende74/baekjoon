aa4af2b90511c620f6288506a95aabfc5a3fa907
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-12 23:43:58 +0900

    [Gold V] Title: 선 긋기, Time: 3604 ms, Memory: 215204 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,13 @@
+N= int(input())
+seq = [tuple(map(int,input().split())) for _ in range(N)]
+seq.sort()
+
+res = 0
+cur_s = seq[0][0]
+cur_e = seq[0][1]
+for s, e in seq[1:]:
+    if s > cur_e:
+        res += cur_e - cur_s
+        cur_s = s
+    cur_e = max(cur_e, e)
+print(res+cur_e-cur_s)
