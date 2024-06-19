2a23ec5c31590cc9bcc5b438d5b33959850695f6
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-20 05:23:02 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 44 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
@@ -1,12 +1,11 @@
-from bisect import bisect
 N, goal = map(int,input().split())
 seq = list(map(int,input().split()))
 seq.sort()
 seq_set = set(seq)
 def solve():
     if goal in seq_set: return True
-    i,j = 0, N-1
-    while i<j:
+    i, j = 0, N-1
+    while i < j:
         s1, s3 = seq[i], seq[j]
         if s1+s3 == goal: return True
         elif s1+s3 > goal: j -= 1
