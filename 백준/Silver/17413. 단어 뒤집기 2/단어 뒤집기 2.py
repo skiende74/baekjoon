30d467c5ef7243a336fc7aa54a715559fdc08e29
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-20 07:28:30 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,35 +1,27 @@
 seq = input()
 N = len(seq)
-cnt = 0
+is_tag = 0
 ans = []
 stack = ''
+
+x = ''
 for i, c in enumerate(seq):
     if c == '<': 
-        cnt += 1
-        if stack:
-            ans.append(stack[::-1])
-            stack = ''
-        stack += '<'
+        is_tag = True
+        x += stack[::-1] + '<'
+        stack = ''
     elif c == '>':
-        cnt -= 1
-        ans.append(stack+'>')
+        is_tag = False
+        x += stack+'>'
         stack = ''
     elif c == ' ':
-        if cnt == 0:
-            ans.append(stack[::-1])
+        if not is_tag:
+            x += stack[::-1] + ' '
             stack = ''
         else:
             stack += ' '
     else:  
         stack += c
-if stack: ans.append(stack[::-1])
+x +=  stack[::-1]
 
-prev_is_tag = True
-answer = ''
-for l in ans:
-    is_tag = l[0] == '<' and l[-1] == '>'
-    if not prev_is_tag and not is_tag:
-        l = ' '+l
-    answer += l
-    prev_is_tag = is_tag
-print(answer)    
+print(x)    
