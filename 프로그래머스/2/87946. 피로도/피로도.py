5904f4cc314763f6182d99f5ddeab6a922c8a5f2
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-22 02:04:51 +0900

    [level 2] Title: 피로도, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,23 @@
-from itertools import permutations
+answer = 0
+N = 0
+visited = []
 
-def solution(k0, dungeons):
-    max_cnt = 0
-    for perm in permutations(dungeons, len(dungeons)):
-        cnt = 0
-        k = k0
-        for min_val, use in perm:
-            if k < min_val: continue
-            k -= use
-            cnt += 1
-        max_cnt = max(max_cnt, cnt)
-    return max_cnt
+
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt > answer:
+        answer = cnt
+
+    for j in range(N):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1
+            dfs(k - dungeons[j][1], cnt + 1, dungeons)
+            visited[j] = 0
+
+
+def solution(k, dungeons):
+    global N, visited
+    N = len(dungeons)
+    visited = [0] * N
+    dfs(k, 0, dungeons)
+    return answer
