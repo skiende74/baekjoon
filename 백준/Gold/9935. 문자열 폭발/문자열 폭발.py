326aa3acdbc4c52a06cd0522620a0e158f42145a
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-21 17:50:11 +0900

    [Gold IV] Title: 문자열 폭발, Time: 340 ms, Memory: 41268 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,9 +1,10 @@
 seq = input()
 bomb = list(input())
 M = len(bomb)
+b = bomb[-1]
 stack = []
 for s in seq:
     stack.append(s)
-    while s==bomb[-1] and stack[-M:] == bomb: del stack[-M:]
+    while s==b and stack[-M:] == bomb: del stack[-M:]
 stack = ''.join(stack)
 print(stack if stack else 'FRULA')
