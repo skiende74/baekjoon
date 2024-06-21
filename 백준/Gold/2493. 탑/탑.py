6b0e628027c9f09f399c7f5b5d6a6238a3a8718d
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-21 16:23:35 +0900

    [Gold V] Title: 탑, Time: 504 ms, Memory: 101620 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,9 +1,9 @@
 N = int(input())
 seq = list(map(int,input().split()))
-stack = [(-1,10**8+2)]
-ans = [0]*N
+stack = []
+ans = [-1]*N
 for i,s in enumerate(seq):
-    while stack and stack[-1][1] <= s: stack.pop()
-    if stack: ans[i] = stack[-1][0]
-    stack.append((i,s))
+    while stack and seq[stack[-1]] <= s: stack.pop()
+    if stack: ans[i] = stack[-1]
+    stack.append(i)
 print(*[s+1 for s in ans])
