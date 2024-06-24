fe745865048ac3c3f4f812b8badb29019ea64a37
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-25 00:05:40 +0900

    [Silver III] Title: N과 M (1), Time: 120 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,4 +1,14 @@
-from itertools import permutations
-N, M = map(int,input().split())
-for c in permutations(range(1,N+1), M):
-    print(*c)
+n, m = map(int,input().split())
+check=[0 for i in range(n+1)]
+
+def permultation(now,m,s):
+    if (now==m):
+        print(s)
+    else:
+        for i in range(1,n+1):
+            if check[i]==0:
+                    check[i]=1
+                    permultation(now+1,m,s+str(i)+' ')
+                    check[i]=0
+
+permultation(0,m,"")
