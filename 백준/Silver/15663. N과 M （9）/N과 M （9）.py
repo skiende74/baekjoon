555d4b8c21b4691d18956ac9deb34b9648380613
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-26 06:28:29 +0900

    [Silver II] Title: N과 M (9), Time: 252 ms, Memory: 51428 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,27 @@
+# 순열. seq도 중복 X
+def dfs():
+    if len(res) == M:
+        ans.append(res.copy())
+        return
+    
+    for j in range(N):
+        if j in res: continue
+        res.append(j)
+        dfs()
+        res.pop()
+
+
+N,M = map(int,input().split())
+seq = list(map(int,input().split()))
+seq.sort()
+
+res, ans = [], []
+dfs()
+
+ans2 = sorted({ tuple(map(lambda i: str(seq[i]), res)) for res in ans })
+used = set()
+for res in ans:
+    r = ' '.join(map(lambda i: str(seq[i]), res))
+    if r in used: continue
+    print(r)
+    used.add(r)
