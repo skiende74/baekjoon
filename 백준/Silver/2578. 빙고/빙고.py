3eced0e6f1cdd134e88ea6ae4617540e6c2de168
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-29 00:10:24 +0900

    [Silver IV] Title: 빙고, Time: 76 ms, Memory: 34076 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+N = 5
+grid = [list(map(int,input().split())) for _ in range(N)]
+calls =[list(map(int,input().split())) for _ in range(N)]
+calls = reduce(lambda r,c: [*r,*c], calls, [])
+
+pose = {grid[i][j]:(i,j)
+ for i in range(N)
+    for j in range(N)}
+
+row,col = [5]*N, [5]*N
+diag1, diag2= 5,5
+cnt = 0
+for k,c in enumerate(calls):
+    i,j = pose[c]
+    row[i] -= 1
+    col[j] -= 1
+    if row[i] == 0: cnt += 1
+    if col[j] == 0: cnt += 1
+    if i==j:
+        diag1 -= 1
+        if diag1 == 0: cnt += 1
+    if N-1-i == j:
+        diag2 -= 1
+        if diag2 == 0: cnt += 1
+    if cnt >= 3:
+        print(k+1)
+        break
