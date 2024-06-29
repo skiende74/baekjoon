4d1d7471b12ee90801e587c05a91c777d4993d95
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-29 21:18:57 +0900

    [Silver III] Title: 선물, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.py
+++ 백준/Silver/1166. 선물/선물.py
@@ -2,7 +2,7 @@ def possible(m): return (L//m)*(W//m)*(H//m)>=N
 N,L,W,H = map(int,input().split())
 
 l, r = 1e-9, min([L,W,H])
-for _ in range(10**4):
+for _ in range(300):
     m = (l+r)/2
     if possible(m): l = m
     else: r = m
