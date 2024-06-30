3c9258ade44bda1c4da08c028f9b56b39e13e414
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-30 21:13:48 +0900

    [Gold IV] Title: 타일 채우기, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -5,16 +5,9 @@ public class Main {
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int dp[] = new int[100];
-        if (N % 2 == 1) {
-            System.out.println(0);
-            s.close();
-            return;
-        }
 
         dp[0] = 1;
-        dp[2] = 3;
-        dp[4] = 9 + 2;
-        for (int i = 6; i <= N; i += 2) {
+        for (int i = 2; i <= N; i += 2) {
             dp[i] = dp[i - 2] * 3;
             for (int j = 0; j <= i - 4; j += 2) {
                 dp[i] += 2 * dp[j];
