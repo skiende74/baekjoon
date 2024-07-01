dc5c86abd88d5a8d3ebf626ceb79df96bd63176b
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-01 09:32:29 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 108 ms, Memory: 37024 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+grid = [[0] * (M+1)]+[[0]+list(map(int,input().split())) for _ in range(N)]
+p_sum = [[0] * (M+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        p_sum[i][j] = -p_sum[i-1][j-1] + grid[i][j] + p_sum[i][j-1] + p_sum[i-1][j]
+
+for _ in range(int(input())):
+    i1, j1, i2, j2 = map(int,input().split())
+    print(p_sum[i2][j2] - p_sum[i2][j1-1] - p_sum[i1-1][j2] + p_sum[i1-1][j1-1])
