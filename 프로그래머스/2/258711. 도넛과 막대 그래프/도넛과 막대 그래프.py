831f3302aa245105ef2209a0a4fafdc5631a5d67
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-03 02:42:24 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,54 +1,37 @@
 from collections import defaultdict
 graph = defaultdict(lambda: [])
 import sys
-sys.setrecursionlimit(1000001)
 
+sys.setrecursionlimit(1000001)
+visited = defaultdict(lambda: False)
 def solution(edges):
-    global visited, has_two_path
-    result = [-1,0,0,0]
-    N = -1
-    for edge in edges:
-        a, b = edge
-        graph[a].append(b)
-        N = max(N, a, b)
-        
-    toward = [True]+[False]*(N)
-    for edge in edges:
-        a, b = edge
-        toward[b] = True
+    global visited, has_two, has_cycle
     
     
-    for i, t in enumerate(toward):
-        if not t and len(graph[i]) >= 2:
-            result[0] = i
-            break
+    for a, b in edges: graph[a].append(b)
+    a = {a for a, b in  edges}
+    b = {b for a, b in  edges}
+    start = list(filter(lambda i: len(graph[i])>=2, a-b))[0]
+    result = [start, 0, 0, 0]
     
-    visited = [False]*(N+1)
-    for i in graph[result[0]]:
-        if not visited[i]:
-            has_two_path = False
-            dfs(i)
-            if len(graph[i]) == 2:
-                result[3] +=1
-            elif len(graph[i]) == 1:
-                if visited[i]:
-                    if has_two_path:
-                        result[3] += 1
-                    else:
-                        result[1] += 1
-                else:
-                    result[2] += 1
-            elif len(graph[i]) == 0:
-                result[2] += 1
-            visited[i] = True
+    for i in graph[start]:
+        visited[i] = True
+        has_two, has_cycle = False, False
+        dfs(i)
+
+        if has_two: result[3] += 1
+        elif has_cycle: result[1] += 1
+        else: result[2] += 1
     return result
 
 def dfs(i):
-    global has_two_path
-    if len(graph[i]) >=2:
-        has_two_path = True
+    global has_two, has_cycle
+    last = i
+    if len(graph[i]) >=2: has_two = True
+    
     for j in graph[i]:
-        if visited[j]: continue
+        if visited[j]: 
+            has_cycle = True
+            continue
         visited[j] = True
         dfs(j)
-    
