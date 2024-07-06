92e3ae07c52d562bda6ce7173e32a5b84c96fed2
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-06 19:57:35 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 336 ms, Memory: 53580 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,19 +1,12 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
+
 N = int(input())
-seq = [list(map(int,input().split())) for _ in range(N)]
+schedules = sorted([list(map(int, input().split())) for _ in range(N)])
+h = []
+for start, end in schedules:
+    if h and h[0] <= start:
+        heapq.heappop(h)
+    heapq.heappush(h, end)
 
-segments = []
-for s,e in seq:
-    segments.append((s,1))
-    segments.append((e,-1))
-
-segments.sort()
-max_cnt, cnt = 0, 0
-prev = 0
-for x, op in segments:
-    if prev != x: max_cnt = max(max_cnt, cnt)
-    cnt += op
-    prev = x
-
-print(max_cnt)
+print(len(h))
