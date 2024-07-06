b741f15458cc286deedc39d20f9cab63b302ead0
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-06 09:21:08 +0900

    [Silver IV] Title: 올바른 배열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1337. 올바른 배열/올바른 배열.py
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -6,8 +6,7 @@ seq_set = set(seq)
 max_cnt, cnt = 1, 1
 
 for i in range(N):
-    for i0 in range(seq[i]-4,seq[i]+1):
-        cnt = len([ j for j in range(i0, i0+5)
-            if j in seq_set])
-        max_cnt = max(max_cnt, cnt)
+    cnt = len([ j for j in range(seq[i], seq[i]+5)
+        if j in seq_set])
+    max_cnt = max(max_cnt, cnt)
 print(5-max_cnt)
