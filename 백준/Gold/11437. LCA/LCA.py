50fcaedd122f3e78060964a5ad42dedd6f754c01
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-20 21:08:03 +0900

    [Gold III] Title: LCA, Time: 1044 ms, Memory: 213848 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.py
+++ 백준/Gold/11437. LCA/LCA.py
@@ -1,34 +1,32 @@
-prev = -1
 def dfs(i):
-    global prev
     for j in graph[i]:
         if depth[j] != 10**9: continue
+        depth[j] = depth[i] + 1
         parent[j] = i
-        depth[j] = depth[i] + 1    
         dfs(j)
-        
 
 def lca(i, j):
-    while depth[i] > depth[j]: i = parent[i]
-    while depth[i] < depth[j]: j = parent[j]
+    while depth[i] > depth[j] : i = parent[i]
+    while depth[i] < depth[j] : j = parent[j]
     while i != j: i, j = parent[i], parent[j]
     return i
-
+    
 import sys
 sys.setrecursionlimit(50000+1)
 input = sys.stdin.readline
 
 N = int(input())
 graph = [[] for _ in range(N+1)]
-for i, j in  [list(map(int,input().split())) for _ in range(N-1)]:
+for _ in range(N-1):
+    i, j = map(int, input().split())
     graph[i].append(j)
     graph[j].append(i)
 
-parent = [i for i in range(N+1)]
 depth = [10**9]*(N+1)
+parent = [0]*(N+1)
 
 depth[1] = 0
+parent[1] = 1
 dfs(1)
 for _ in range(int(input())):
-    i, j = map(int,input().split())
-    print(lca(i,j))
+    print(lca(*map(int, input().split())))
