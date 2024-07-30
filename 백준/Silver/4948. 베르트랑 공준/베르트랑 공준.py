203d1ab44986d17f6631c7aeb26ebb1e53d97520
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-30 20:59:05 +0900

    [Silver II] Title: 베르트랑 공준, Time: 188 ms, Memory: 34528 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,32 +1,13 @@
-def makePrimeNumbers(maxNumber):
-    primeNumbers={i for i in range(2,maxNumber+1)}
+N = 123456*2
 
-    for i in range(2,maxNumber+1):
-        if i not in primeNumbers:
-            continue
-        for j in range(2,(maxNumber+1)//i+2):
-            if i*j in primeNumbers:
-                primeNumbers.remove(i*j)
-    return primeNumbers
+is_prime = [True]*(N+1)
+is_prime[1] = False
 
-def processInputs():
-    inputNumbers=[]
-    while True:
-        N=int(input())
-        if N != 0:
-            inputNumbers.append(N)
-        else:
-            break
-    return inputNumbers
+for i in range(2, N+1):
+    if not is_prime[i]: continue
+    for j in range(2*i, N+1, i): is_prime[j] = False
 
-def calculateCountsOfNumbersWithBertrandsPostulate(inputNumber):
-    count=0
-    for testCase in range(inputNumber+1,inputNumber*2+1):
-        if testCase in primeNumbers:
-            count+=1
-    return count
-primeNumbers = makePrimeNumbers(123456*2)
-inputNumbers = processInputs()
-
-for inputNumber in inputNumbers:
-    print(calculateCountsOfNumbersWithBertrandsPostulate(inputNumber))
+while True:
+    n = int(input())
+    if n == 0: break
+    print(sum(is_prime[n+1:2*n+1]))
