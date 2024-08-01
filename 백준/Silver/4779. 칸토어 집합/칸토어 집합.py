aa81e39cdaa2f5bf8eea994ef25f7b25074f6fbc
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-08-02 02:53:09 +0900

    [Silver III] Title: 칸토어 집합, Time: 44 ms, Memory: 32500 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,16 +1,10 @@
-def recursive(N, K):
-    if N == 1 and (K==1 or K==3): 
-        print('-', end='')
-        return
-    if K == 2: 
-        print(' '*(3**(N-1)), end='')
-        return
-    recursive(N-1, 1)
-    recursive(N-1, 2)
-    recursive(N-1, 3)
-
+def recursive(N):
+    if N == 0: return '-'
+    p = recursive(N-1)
+    return p + ' '*(3**(N-1)) + p
+    
 import sys
 lines = list(map(int, sys.stdin.read().split()))
 for n in lines:
-    recursive(n+1, 1)
-    print()
+    print(recursive(n))
+    
