70bdff9c6b2237bf79f2b016048e760dee8f5071
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-08-20 21:28:58 +0900

    [Gold IV] Title: 사탕 가게, Time: 1524 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.py
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.py
@@ -1,29 +1,16 @@
+#이게 맞음
 import sys
+lines = sys.stdin.read().split('\n')[:-1]
 
-input = sys.stdin.readline
-
-def solution():
-    while 1:
-        n, m = input().split()
-        n, m = int(n), int(float(m)*100+0.5)
-        if n == m == 0:
-            break
-        temp = []
-        for _ in range(n):
-            c, p = input().split()
-            c, p = int(c), int(float(p)*100+0.5)
-            temp.append((c, p))
-        temp.sort()
-        candies = [temp[0]]
-        for i in range(1, n):
-            if temp[i][0] > candies[-1][0]:
-                candies.append(temp[i])
-        dp = [0]*(m+1)
-        for c, p in candies:
-            for i in range(p, m+1):
-                cnt = dp[i-p]+c
-                if cnt > dp[i]:
-                    dp[i] = cnt
-        print(dp[-1])
-
-solution()
+k = 0
+while k<len(lines)-1:
+    N, M = lines[k].split()
+    N, M = int(N), int(float(M)*100+0.5)
+    dp = [0]*(M+1)
+    for line in lines[k+1:k+N+1]:
+        c, p = line.split()
+        c, p = int(c), int(float(p)*100+0.5)
+        for i in range(p, M+1):
+            dp[i] = max(dp[i], dp[i-p]+c)
+    print(max(dp))
+    k += N+1
