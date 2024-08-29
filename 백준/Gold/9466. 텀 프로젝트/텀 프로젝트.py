83fe61bdaa73f2d69f27f29894ff1261e9d3d383
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-08-30 00:25:35 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1948 ms, Memory: 47804 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,28 +1,34 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**5+2)
-def dfs(i):
-    global ans
-    j = seq[i]
-    if not visited[j]: 
-        visited[j] = True
-        dfs(j)
-    elif not finished[j]:
-        k = j
-        while k != i:
-            k = seq[k]
-            ans += 1
-        ans += 1
-    finished[i] = True     
-    
+def find_teamless_students(n, selections):
+    visited = [0] * (n + 1)  # 방문 여부를 체크하기 위한 리스트
+    finished = [0] * (n + 1)  # 팀 결정 여부 체크
+    result = 0
+
+    def dfs(student):
+        nonlocal result
+        current = student
+        path = []
+
+        while not visited[current]:
+            visited[current] = 1
+            path.append(current)
+            current = selections[current - 1]
+
+        if current in path:  # 사이클이 형성된 경우
+            result += len(path[path.index(current):])  # 사이클의 길이만큼 결과에 더하기
+
+        for node in path:
+            finished[node] = 1  # 경로에 있는 모든 노드를 팀 결정으로 표시
+
+    for i in range(1, n + 1):
+        if not visited[i]:
+            dfs(i)
+
+    return n - result
+
 for _ in range(int(input())):
     N = int(input())
-    seq = [0]+list(map(int,input().split()))
-    visited = [False]*(N+1)
-    finished = [False]*(N+1)
-    ans = 0
-    for i in range(1, N+1):
-        if visited[i]: continue
-        visited[i] = True
-        dfs(i)
-    print(N-ans)
+    seq = list(map(int,input().split()))
+    print(find_teamless_students(N, seq))
