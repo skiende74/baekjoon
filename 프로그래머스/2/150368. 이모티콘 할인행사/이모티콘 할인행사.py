cbbb48c9aed4c2038e4c5b8497c46c650787cd79
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-10-05 03:15:57 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1768.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,30 +1,19 @@
-# 유저, 상품들, 결과.
-# 유저당, (상품당, 할인율10/20/30/40)
-import sys
-sys.setrecursionlimit(10**7)
-
-def dfs(users, emoticons):
-    global result_max  
-    if len(discounts) == len(emoticons):
-        result = [0,0]
-        for discount_need, upper_price in users:
-            purchase = 0
-            for i in range(len(emoticons)):
-                purchase += int((100-discounts[i])*0.01*emoticons[i]) if discounts[i] >= discount_need else 0
-                
-            if purchase >= upper_price: result[0] += 1
-            else: result[1] += purchase
-        result_max = max(result_max, result)
-        return
-        
-    for j in range(10, 50, 10):
-        discounts.append(j)
-        dfs(users, emoticons)
-        discounts.pop()
-    
 def solution(users, emoticons):
-    global result_max, discounts
-    result_max = [0,0]
-    discounts = []
-    dfs(users, emoticons)
-    return result_max
+    results=[]
+    def dfs(discounts):
+        if len(discounts)==7:
+            return calc(discounts)
+        return max( [ dfs([*discounts, r]) for r in [10,20,30,40] ] )
+    
+    def calc(discounts):
+        result = [0, 0]
+        for rate, money in users:
+            sales = 0
+            for price, discount in zip(emoticons, discounts):
+                if discount >= rate:  sales += price*(1-discount*0.01)
+            if sales >= money:  result[0] += 1
+            else:  result[1] += sales
+                    
+        return result
+    return dfs([])
+    
