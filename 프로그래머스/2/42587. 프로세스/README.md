70850be56967716201135a320954fcbf061aff87
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-10-28 19:51:43 +0900

    [level 2] Title: 프로세스, Time: 0.54 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.92 ms
+메모리: 33.6 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 28일 19:51:08
+2024년 10월 28일 19:51:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,14 +1,11 @@
 function solution(priorities, location) {
     let cnt = 0;
-    let pp = priorities.map((el,idx)=>[el,idx]);
-    let pop = [-1,-1];
-    while (pop[1] !== location) {
-        const M = Math.max(...pp.map(x=>x[0]));
-        const maxIdx = pp.findIndex(el=>el[0]===M);
-        pop = pp[maxIdx];
-        pp = [...pp.slice(maxIdx+1), ...pp.slice(0,maxIdx)];
-        
-        cnt += 1;
+    let maxIdx = -1;
+    while (maxIdx !== location) {
+    const M = Math.max(...priorities);
+    maxIdx = priorities.findIndex(el=>el===M);
+    priorities = [...priorities.slice(maxIdx+1),...priorities.slice(0,maxIdx)];
+    cnt += 1;
     }
     return cnt;
 }
