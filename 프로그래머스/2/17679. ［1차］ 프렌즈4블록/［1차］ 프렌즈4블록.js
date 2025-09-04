a3ba005e44e2161696045b597840d0497326cafe
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2025-09-04 19:16:39 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 17.41 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,69 +1,42 @@
-function solution2(m, n, board) {
-    const explode = new Set();
-    for (let i=0; i<m-1;i++){
-        for (let j=0; j<n-1;j++){
-            const ds = [[0,0],[0,1],[1,0],[1,1]];
-            const box = ds.map(([di,dj])=>[i+di,j+dj]);
-            const cnt = box.filter(([i2,j2])=>board[i2][j2]===board[i][j]).length;
-            if (cnt===4) box.forEach(([i2,j2])=>explode.add([i2,j2].join(',')));
-        }
-    }
-    console.log(explode)
-    return explode;
-}
-
 function solution(m, n, board) {
-  // 문자 배열로 변환
-    const grid = board.map(row => row.split(''));
-    let totalRemoved = 0;
+  const g = board.map(r => r.split(''));
+  let removed = 0;
 
-    const inRange = (y, x) => y + 1 < m && x + 1 < n;
+  while (true) {
+    const mark = Array.from({ length: m }, () => Array(n).fill(false));
+    let hit = false;
 
-    while (true) {
-        // 1) 지울 칸 표시용 boolean 배열
-        const mark = Array.from({ length: m }, () => Array(n).fill(false));
-        let isFound = false;
-        for (let i = 0; i<m-1 ; i++){
-            for (let j = 0; j<n-1; j++){
-                const c = grid[i][j]
-                if (c===' ')continue;
-                const i2j2s = [[0,0],[0,1],[1,0],[1,1]]
-                               .map(([di,dj])=>[i+di,j+dj]);
-                const isExplode = i2j2s
-                                  .filter(([i2,j2])=>grid[i2][j2] === grid[i][j]).length === 4;
-                if (isExplode) {
-                    i2j2s.forEach(([i2,j2])=>{mark[i2][j2] = true});
-                    isFound = true;
-                }
-            }
+    // 2x2 탐지 (상수 4칸 비교)
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        const c = g[i][j];
+        if (!c) continue; // 빈칸이면 스킵
+        if (g[i][j+1] === c && g[i+1][j] === c && g[i+1][j+1] === c) {
+          mark[i][j] = mark[i][j+1] = mark[i+1][j] = mark[i+1][j+1] = true;
+          hit = true;
         }
-      
+      }
+    }
+    if (!hit) break;
 
-        if (!isFound) break; // 더 이상 지울 2x2 없음
+    // 지우고 개수 집계
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (mark[i][j]) { g[i][j] = null; removed++; }
+      }
+    }
 
-        const next = Array.from({length:m}, ()=>Array(n).fill(' '));
-        // 3) 표시된 칸 지우고 개수 세기
-        for (let j = 0; j<n; j++){
-            let write = m-1;
-            for (let i = m-1; i>=0; i--){
-                if (!mark[i][j]){
-                    next[write][j] = grid[i][j];
-                    write -= 1;
-                }          
-                else{
-                    totalRemoved += 1;
-                }
-            }
-        }
-        
-        for (let i = 0; i<m; i++){
-            for (let j = 0; j<n; j++){
-                grid[i][j] = next[i][j];
-            }
-        }
-        //return totalRemoved;
+    // next로 낙하(열별 아래부터 채우기)
+    const next = Array.from({ length: m }, () => Array(n).fill(null));
+    for (let j = 0; j < n; j++) {
+      let w = m - 1;
+      for (let i = m - 1; i >= 0; i--) {
+        if (g[i][j] != null) next[w--][j] = g[i][j];
+      }
+    }
+    // 스왑(얕은 복사)
+    for (let i = 0; i < m; i++) g[i] = next[i];
   }
- 
-    
-  return totalRemoved;
+
+  return removed;
 }
