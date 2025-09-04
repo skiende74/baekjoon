61a187219396bca65e2997625e5a59671155026d
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2025-09-04 18:59:50 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 90.94 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -14,62 +14,56 @@ function solution2(m, n, board) {
 
 function solution(m, n, board) {
   // 문자 배열로 변환
-  const grid = board.map(row => row.split(''));
-  let totalRemoved = 0;
+    const grid = board.map(row => row.split(''));
+    let totalRemoved = 0;
 
-  const inRange = (y, x) => y + 1 < m && x + 1 < n;
+    const inRange = (y, x) => y + 1 < m && x + 1 < n;
 
-  while (true) {
-    // 1) 지울 칸 표시용 boolean 배열
-    const mark = Array.from({ length: m }, () => Array(n).fill(false));
-    let found = false;
-
-    // 2) 2x2 같은 블록 찾기
-    for (let y = 0; y < m - 1; y++) {
-      for (let x = 0; x < n - 1; x++) {
-        const c = grid[y][x];
-        if (!c) continue; // 이미 빈칸(null)
-        if (
-          inRange(y, x) &&
-          grid[y][x + 1] === c &&
-          grid[y + 1][x] === c &&
-          grid[y + 1][x + 1] === c
-        ) {
-          mark[y][x] = mark[y][x + 1] = mark[y + 1][x] = mark[y + 1][x + 1] = true;
-          found = true;
+    while (true) {
+        // 1) 지울 칸 표시용 boolean 배열
+        const mark = Array.from({ length: m }, () => Array(n).fill(false));
+        let isFound = false;
+        for (let i = 0; i<m-1 ; i++){
+            for (let j = 0; j<n-1; j++){
+                const c = grid[i][j]
+                if (c===' ')continue;
+                const i2j2s = [[0,0],[0,1],[1,0],[1,1]]
+                               .map(([di,dj])=>[i+di,j+dj]);
+                const isExplode = i2j2s
+                                  .filter(([i2,j2])=>grid[i2][j2] === grid[i][j]).length === 4;
+                if (isExplode) {
+                    i2j2s.forEach(([i2,j2])=>{mark[i2][j2] = true});
+                    isFound = true;
+                }
+            }
         }
-      }
-    }
+      
 
-    if (!found) break; // 더 이상 지울 2x2 없음
+        if (!isFound) break; // 더 이상 지울 2x2 없음
 
-    // 3) 표시된 칸 지우고 개수 세기
-    let removedThisRound = 0;
-    for (let y = 0; y < m; y++) {
-      for (let x = 0; x < n; x++) {
-        if (mark[y][x]) {
-          grid[y][x] = null;
-          removedThisRound++;
+        const next = Array.from({length:m}, ()=>Array(n).fill(' '));
+        // 3) 표시된 칸 지우고 개수 세기
+        for (let j = 0; j<n; j++){
+            let write = m-1;
+            for (let i = m-1; i>=0; i--){
+                if (!mark[i][j]){
+                    next[write][j] = grid[i][j];
+                    write -= 1;
+                }          
+                else{
+                    totalRemoved += 1;
+                }
+            }
         }
-      }
-    }
-    totalRemoved += removedThisRound;
-
-    // 4) 중력: 각 열을 아래로 떨어뜨리기
-    for (let x = 0; x < n; x++) {
-      // 아래에서 위로 읽어와 non-null만 모아 쌓는다
-      let write = m - 1;
-      for (let y = m - 1; y >= 0; y--) {
-        if (grid[y][x] !== null) {
-          grid[write][x] = grid[y][x];
-          if (write !== y) grid[y][x] = null;
-          write--;
+        
+        for (let i = 0; i<m; i++){
+            for (let j = 0; j<n; j++){
+                grid[i][j] = next[i][j];
+            }
         }
-      }
-      // 나머지 위쪽은 null로 채우기(이미 되어있지만 안전하게)
-      for (let y = write; y >= 0; y--) grid[y][x] = null;
-    }
+        //return totalRemoved;
   }
-
+ 
+    
   return totalRemoved;
 }
